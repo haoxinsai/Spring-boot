0576517211a0f935c42fdc901abac19bdc7746c8
haoxinsai <313299233@qq.com>
2019-11-06 19:21:01 +0800

    1-SpringBoot

---
+++ src/main/java/com/test/tests_b/TestsBApplication.java
@@ -0,0 +1,13 @@
+package com.test.tests_b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestsBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestsBApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/test/tests_b/TestsBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.tests_b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestsBApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
