293ed0e82bd09570edaf0ec0b6d1bdd5247dae41
haoxinsai <313299233@qq.com>
2019-11-15 22:55:33 +0800

    从github获取信息插入到数据库中

--- README.md
+++ README.md
@@ -7,7 +7,9 @@
 [Github 项目本地址](https://github.com/haoxinsai/Spring-boot)
 [GIthub创建一个OAuth应用](https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/)
 [maven网站](https://mvnrepository.com)
-
+[H2](http://www.h2database.com/html/main.html)
+[Mybatis-SpringBoot](https://mybatis.org/spring-boot-starter/mybatis-spring-boot-autoconfigure/index.html)
+[Spring Boot](https://docs.spring.io/spring-boot/docs/2.0.0.RC1/reference/htmlsingle/#boot-features-embedded-database-support)
 
 
 
@@ -16,19 +18,45 @@
 [Visual-Paradigm](https://www.visual-paradigm.com)
 [okHttp](https://square.github.io/okhttp/)
 [Fastjson](https://mvnrepository.com/artifact/com.alibaba/fastjson)
-
+[H2](https://mvnrepository.com/artifact/com.h2database/h2/1.4.200)
 ##MAVEN包
+````
 okHttp:
         <dependency>
           <groupId>com.squareup.okhttp3</groupId>
           <artifactId>okhttp</artifactId>
           <version>3.14.1</version>
         </dependency>
-
+````
+````
 Fastjson:
          <dependency>
            <groupId>com.alibaba</groupId>
            <artifactId>fastjson</artifactId>
            <version>1.2.57</version>
          </dependency>
+````
+````
+h2:
+        <dependency>
+            <groupId>com.h2database</groupId>
+            <artifactId>h2</artifactId>
+            <version>1.4.200</version>
+        </dependency>
+ ````     
+ ````     
+mysql
+        <dependency>
+            <groupId>mysql</groupId>
+            <artifactId>mysql-connector-java</artifactId>
+            <version>5.1.31</version>
+        </dependency>
+    spring.datasource.url=jdbc:mysql://localhost:3306/test?useUnicode=true&amp;characterEncoding=utf8&amp;rewriteBatchedStatements=true
+    spring.datasource.username=root
+    spring.datasource.password=jswkys123456
+    spring.datasource.driver-class-name=com.mysql.jdbc.Driver  
+  ````      
+        
+      
+
 
--- src/main/java/com/test/tests_b/controller/AuthorizeController.java
+++ src/main/java/com/test/tests_b/controller/AuthorizeController.java
@@ -2,18 +2,27 @@ package com.test.tests_b.controller;
 
 import com.test.tests_b.dto.AccessTokenDTO;
 import com.test.tests_b.dto.GithubUser;
+import com.test.tests_b.mapper.UserMapper;
+import com.test.tests_b.model.User;
 import com.test.tests_b.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
 
     @Autowired
     private GithubProvider githubProvider;
+
+    @Autowired
+    private UserMapper userMapper;
 
     @Value("${github.client.id}")
     private String clientId;
@@ -24,7 +33,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code")String code,
-                           @RequestParam(name="state")String state){
+                           @RequestParam(name="state")String state,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -32,8 +42,21 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if(githubUser != null){
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+           user.setGmtModified(user.getGmtCreate());
+           userMapper.insert(user);
+            //登陆成功获取cookie和session
+            request.getSession().setAttribute("user" , githubUser);
+            System.out.println(githubUser.getName());
+            return "redirect:/";
+        }else{
+            return "redirect:/";
+        }
     }
 }
